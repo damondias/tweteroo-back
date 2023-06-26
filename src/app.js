bc50e9ea47d6f8b243fd659858515d898693e69f
Damon <damondias@gmail.com>
2023-06-26 01:25:37 -0400

    feat: Sign-in Post Route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
 
 const users = [];
 const tweets = [];
 
 const app = express();
 
+app.use(json());
+app.use(cors());
+
+app.post("/sign-up", (req,res) => {
+    const body = req.body;
+    users.push(body);
+    res.send('Ok');
+});
+
 app.listen(5000, () => {
-    console.log(`Running API tweteroo on port 5000, 
-                 Url: http://localhost:5000`)
+    console.log(`
+                Running API Tweteroo on port 5000,
+                Url: http://localhost:5000`);
 })
