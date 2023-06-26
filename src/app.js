8c198e37e60f071f333d1672b624e21fed39c005
Damon <damondias@gmail.com>
2023-06-26 16:16:48 -0400

    feat: Validation of data and status code and POST of tweets by username

--- src/app.js
+++ src/app.js
@@ -10,28 +10,36 @@ app.use(json());
 app.use(cors());
 
 app.post("/sign-up", (req,res) => {
-    const body = req.body;
+    const {username, avatar} = req.body;
+    if(!username || !avatar) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+      }
 
-    users.push(body);
-    res.send('Ok');
+    users.push({username, avatar});
+    res.status(201).send('Ok');
 });
 
 app.post("/tweets", (req,res) => {
     const body = req.body;
-    
     if (!users.find(u => u.username === body.username)) {
         res.status(401).send("UNAUTHORIZED");
         return
     }
 
     const { username, tweet} = body;
+    if(!username || !tweet) {
+        res.status(400).send("Todos os campos são obrigatórios!");
+        return;
+    }
+
     const { avatar } = users.find(u => u.username === username)
     tweets.push({
         username,
         avatar,
         tweet,
     });
-    res.send('Ok');
+    res.status(201).send('Ok');
 });
 
 app.get("/tweets", (req,res) =>{
@@ -42,6 +50,13 @@ app.get("/tweets", (req,res) =>{
         res.send([...tweets].reverse().splice(0,10));
     }
 });
+
+app.get("/tweets/:username", (req, res) => {
+    const {username} =  req.params;
+    const tweetsFiltrados = tweets.filter(tweet => tweet.username === username);
+    res.send(tweetsFiltrados);
+});
+
 
 app.listen(5000, () => {
     console.log(`
